108b8a4340a96384c8e6ece4555f6a0cf775f49b
Rodrigo Trujillo <rootrujo@gmail.com>
2018-10-15 20:07:49 -0500

    Operadores logicos

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,59 @@
-alert("Hello")
+// Forma de comentar en JS
+
+/*
+ Múltiples líneas 
+*/
+
+
+/**
+ * Multimples lineas con astericos que den formato
+ * Multimples lineas con astericos que den formato
+ */
+
+// TIPOS DE DATOS  #5
+
+// Number
+var numero = 10;
+
+// String caracteres 
+var string = "Esto es un string";
+var string2 = 'Esto es un string con comillas simples';
+var string3 =  `Esto es un string nuevo`;
+
+// Boolean
+var boolean1 = true;
+var boolean2 = false;
+
+// Null
+var vacio = null;
+
+// Undefined
+var indefinido = undefined;
+
+/**
+ * OPERADORES ARITMETICOS
+ */
+
+ var suma = 3 + 6;
+ var resta = 3 - 6;
+ var multiplicacion = 3 * 6;
+ var division = 5 / 2;
+
+ // Modulo
+ var modulo = 5 % 2;
+
+ /**
+  * OPERADORES LOGICOS
+  */
+
+  var mayorQue = 9 > 8;
+  var menorQue = 7 < 4;
+  var mayorQueoIgual = 7 >= 7;
+  var menorQueoIgual = 4 <= 3;
+  var igual = 4 == 4;
+  var desigual = 4 != 4;
+
+  var valorachecar = 20;
+  var esvalido = (valorachecar <=20) || (valorachecar >=30)
+  var esvalido = (valorachecar <=20) && (valorachecar >=30)
+  console.log ("Es valido? ", esvalido);
